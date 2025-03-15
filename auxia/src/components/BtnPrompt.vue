ae932c946a555ff48e50b10a1cd4733cdbad1391
Joice-Araujo <joice.aparecida21@hotmail.com>
2025-03-14 21:47:58 -0300

    :tada: feat input prompt componente

---
+++ auxia/src/components/BtnPrompt.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="botao">
+      <i class="pi pi-arrow-right"></i>
+    </button>
+</template>
+
+<style scoped>
+.botao {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%; 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color:  black;
+  color: white;
+  border: none;
+  font-size: 24px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.botao:hover {
+  background-color: #4F4F4F;
+}
+</style>
