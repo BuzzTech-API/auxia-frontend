3991ccd02345c1f66a2f08139aaa641aa0515456
Joice-Araujo <joice.aparecida21@hotmail.com>
2025-03-24 21:52:36 -0300

    :sparkles: componente de espera da resposta da requisição e parte da rota para a tela de respostas

---
+++ auxia/src/components/LoadingRespostas.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="espera">
+        
+        <i class="pi pi-spin pi-spinner" style="font-size: 4rem; color: #a1a1a1;"></i>
+        <h3>Carregando Respostas</h3>
+    </div>
+
+</template>
+
+<style scoped>
+.espera{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+h3{
+    color: #a1a1a1;
+}
+
+</style>
--- auxia/src/router/index.ts
+++ auxia/src/router/index.ts
@@ -11,6 +11,11 @@ const router = createRouter({
       name: 'home',
       component: () => import('../views/ChatView.vue'),
     },
+    {
+      path: '/lalala',
+      name: 'lalala',
+      component: () => import('../views/Lalala.vue'),
+    },
   ],
 })
 
