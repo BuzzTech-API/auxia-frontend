616402ae71b23d5baa41b813d4b481e088b6ade9
Pedro Davi <pedro.henrique04896@gmail.com>
2025-05-15 00:02:44 -0300

    Criação do botão acessar e inputs de login

---
+++ auxia/src/components/buttons/BtnAcessar.vue
@@ -0,0 +1,41 @@
+<template>
+    <button class="btn-login" @click="handleLogin">
+      Acessar
+      <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15" stroke="white" stroke-width="1.5" fill="none">
+        <path d="M1 8h11.793l-3.147-3.146M12.793 8.5L9.646 11.646" stroke-linecap="round" stroke-linejoin="round" />
+      </svg>
+    </button>
+  </template>
+  
+  <script setup>
+  function handleLogin() {
+    console.log("Fazendo login...");
+  }
+  </script>
+  
+  <style scoped>
+  .btn-login {
+    display: inline-flex;
+    width: 9rem;
+    align-items: center;
+    gap: 19px;
+    background-color: #9800E0;
+    color: white;
+    padding: 6px 6px 6px 15px;
+    border: none;
+    border-radius: 10px;
+    font-size: 17px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+  
+  .btn-login:hover {
+    background-color: #7a00b5;
+  }
+  
+  .icon {
+    width: 28px;
+    height: 28px;
+  }
+  </style>
+  
---
+++ auxia/src/components/inputs/InputEmail.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="input-group">
+      <label for="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        placeholder="email.example@gmail.com"
+        class="input"
+      />
+    </div>
+  </template>
+  
+  <style scoped>
+  .input-group {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 16px;
+  }
+  
+  label {
+    font-weight: bold;
+    margin-bottom: 4px;
+  }
+  
+  .input {
+    padding: 12px;
+    border: none;
+    width: 16rem;
+    border-radius: 8px;
+    background-color: #f9f9f9;
+    color: #333;
+  }
+  </style>
+  
