0626a0d7f28ca76e5c2acaffcf395290125f34ec
Pedro Davi <pedro.henrique04896@gmail.com>
2025-05-15 00:02:44 -0300

    Criação do botão acessar e inputs de login

---
+++ auxia/src/components/inputs/InputSenha.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="input-group">
+      <label for="password">Senha</label>
+      <input
+        type="password"
+        id="password"
+        placeholder="myexamplepassword"
+        class="input"
+      />
+    </div>
+  </template>
+  
+  <style scoped>
+  .input-group {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 24px;
+  }
+  
+  label {
+    font-weight: bold;
+    margin-bottom: 4px;
+  }
+  
+  .input {
+    padding: 12px;
+    border: none;
+    width: 16rem;
+    border-radius: 8px;
+    background-color: #f9f9f9;
+    color: #333;
+  }
+  </style>
+  
